dfb9ffae841f15932f1dc6a8d1f9a41f7b35b73b
z0vs <z0vs@prontonmail.com>
2018-06-15 11:45:13 +0200

    Player sheet added

---
+++ Pizza.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Pizza_body_entered(body):
+	if body is Player:
+		pass
+	
